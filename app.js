d80a30ca240f2466cea0e0c64552ba37a8ee56c4
Stephen Orban <sorban@gmail.com>
2012-04-14 22:22:37 -0400

    in memory blog and posting via /blog/new

---
+++ app.js
@@ -0,0 +1,62 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+  var ArticleProvider = require('./articleProvider-memory').ArticleProvider;
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+var articleProvider = new ArticleProvider();
+app.get('/', function(req, res) {
+  articleProvider.findAll(function(error, docs){
+    res.render('index.jade', { locals: {
+      title: 'Blog',
+      articles:docs
+    }})
+  });
+});
+
+app.get('/blog/new', function(req, res) {
+  res.render('blog_new.jade', { locals: {
+    title: 'New Post'
+  }
+  });
+});
+
+app.post('/blog/new', function(req, res) {
+  articleProvider.save({
+      title: req.param('title'),
+      body: req.param('body')
+    }, function(error, docs) {
+      res.redirect('/')
+    }
+  );
+});
+
+app.listen(8888, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
